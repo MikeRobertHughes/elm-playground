d83f1f3849c9e1004c5fed2de2c452d5495a92f9
Mike Hughes <hughes.mike.r@gmail.com>
2016-09-12 22:18:12 -0400

    Refactor code and get rid of unnecessary parentheses

--- codeCentricPart5.elm
+++ codeCentricPart5.elm
@@ -11,13 +11,13 @@ incrementAll list = List.map (\ n -> n + 1) list
 incrementAll2 = List.map (\ c -> c +1)
 
 main = Html.p []
-    [ Html.text ("3 x 5    = " ++ (toString (multiply 3 5))),
+    [ Html.text <| "3 x 5    = " ++ (toString <| multiply 3 5),
       Html.br [] [],
-      Html.text ("4²       = " ++ (toString (square 4))),
+      Html.text <| "4²       = " ++ (toString <| square 4),
       Html.br [] [],
-      Html.text ("2² x 3²  = " ++ (toString (productOfSquares 2 3))),
+      Html.text <| "2² x 3²  = " ++ (toString <| productOfSquares 2 3),
       Html.br [] [],
-      Html.text ("incrementAll [1, 2, 3] = " ++ (toString (incrementAll [1,2,3]))),
+      Html.text <| "incrementAll [1, 2, 3] = " ++ (toString <| incrementAll [1,2,3]),
       Html.br [] [],
-      Html.text ("incrementAll2 [1, 2, 3] = " ++ (toString (incrementAll2 [1,2,3])))
+      Html.text <| "incrementAll2 [1, 2, 3] = " ++ (toString <| incrementAll2 [1,2,3])
     ]
