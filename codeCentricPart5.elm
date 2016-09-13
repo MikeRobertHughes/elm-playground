95105476acb9733ff0d1bcba64ae6e9cc276b133
Mike Hughes <hughes.mike.r@gmail.com>
2016-09-12 22:13:31 -0400

    create codeCentricPart5 file to demo basic functions

---
+++ codeCentricPart5.elm
@@ -0,0 +1,23 @@
+import Html
+
+multiply a b = a * b
+
+square a = multiply a a
+
+productOfSquares a b = multiply (square a) (square b)
+
+incrementAll list = List.map (\ n -> n + 1) list
+
+incrementAll2 = List.map (\ c -> c +1)
+
+main = Html.p []
+    [ Html.text ("3 x 5    = " ++ (toString (multiply 3 5))),
+      Html.br [] [],
+      Html.text ("4²       = " ++ (toString (square 4))),
+      Html.br [] [],
+      Html.text ("2² x 3²  = " ++ (toString (productOfSquares 2 3))),
+      Html.br [] [],
+      Html.text ("incrementAll [1, 2, 3] = " ++ (toString (incrementAll [1,2,3]))),
+      Html.br [] [],
+      Html.text ("incrementAll2 [1, 2, 3] = " ++ (toString (incrementAll2 [1,2,3])))
+    ]
